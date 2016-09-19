c9e07bcfdbce77a933440d291d89ce59eb29be2c
kiefferbp <kiefferbp@gmail.com>
2016-09-19 12:35:30 -0500

    slightly faster grabber
    We don't need to convert to an int array, though.

--- PixelGrabberTest.java
+++ PixelGrabberTest.java
@@ -3,39 +3,49 @@ import java.awt.image.ImageObserver;
 import java.awt.image.PixelGrabber;
 import java.awt.Toolkit;
 import java.util.HashSet;
+import javax.imageio.ImageIO;
+import java.awt.image.DataBufferByte;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class PixelGrabberTest {
-	public static final int WIDTH = 2560;
-	public static final int HEIGHT = 1600;
-
-	public static boolean isGrayScaleImage(PixelGrabber pg) {
-		return pg.getPixels() instanceof byte[];
-	}
-	
 	public static void main(String[] args) {
+		// compute the starting time
 		final long startTime = System.nanoTime();
-		final int[] pixels = new int[WIDTH * HEIGHT];
-		final Image img = Toolkit.getDefaultToolkit().getImage("circles_2560-1600.png");
-		final PixelGrabber pg = new PixelGrabber(img, 0, 0, WIDTH, HEIGHT, pixels, 0, WIDTH);
-		final HashSet<Integer> pixelSet = new HashSet<>();
 		final long endTime;
 		
+		// get the image
+		final File imgFile = new File("circles_2560-1600.png");
+		
 		try {
-			if (!pg.grabPixels()) {
-				System.err.println("Pixel grab failed!");
-				return;
-			}
-
-			for (final int pixel : pixels) {
-				pixelSet.add(pixel);
-			}
+			final BufferedImage img = ImageIO.read(imgFile);
 			
-			System.out.println(pixelSet);
-
+			// set up the pixel array and set
+			final byte[] subpixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
+			final int[] pixels = new int[img.getWidth() * img.getHeight()];
+			final HashSet<Integer> pixelSet = new HashSet<>();
+			
+			if (img.getAlphaRaster() == null) { // image has no alpha channel
+				for (
+					int subpixel = 0, row = 0, col = 0;
+					subpixel < subpixels.length;
+					subpixel += 3 // since each pixel has 3 subpixels (RGB)
+				) {
+					pixels[subpixel / 3] = -16777216 // 100% alpha (no transparency)
+							+ ((int) subpixels[subpixel] & 0xff) // blue
+							+ (((int) subpixels[subpixel + 1] & 0xff) << 8) // green
+							+ (((int) subpixels[subpixel + 2] & 0xff) << 16); // red
+				}
+			} else {
+				throw new Exception("Unexpected transparency.");
+			} 
+			
 			endTime = System.nanoTime();
 			System.out.println("Duration: " + (int)((endTime - startTime)/1000000) + " ms");
-			
-		} catch (InterruptedException ex) {
+		} catch (IOException ex) {
+			System.err.println("Bad I/O: " + ex);
+		} catch (Exception ex) {
 			System.err.println(ex);
 		}
 	}
